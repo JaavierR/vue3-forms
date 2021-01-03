96e8a60961ddcf5a53981535183f937909632af7
Javier <javier.reys@gmail.com>
2021-01-03 02:58:53 -0300

    feat: add Base components: input, select and checkbox.

---
+++ src/views/SimpleForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form>
+      <BaseSelect
+        :options="categories"
+        v-model="event.category"
+        label="Select a category"
+      />
+
+      <h3>Name & describe your event</h3>
+
+      <BaseInput v-model="event.title" type="text" label="Title" />
+
+      <BaseInput v-model="event.description" type="text" label="Description" />
+
+      <h3>Where is your event?</h3>
+
+      <BaseInput v-model="event.location" type="text" label="Location" />
+
+      <h3>Are pets allowed?</h3>
+      <div>
+        <input type="radio" v-model="event.pets" :value="1" name="pets" />
+        <label>Yes</label>
+      </div>
+
+      <div>
+        <input type="radio" v-model="event.pets" :value="0" name="pets" />
+        <label>No</label>
+      </div>
+
+      <h3>Extras</h3>
+      <div>
+        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
+      </div>
+
+      <div>
+        <BaseCheckbox v-model="event.extras.music" label="Live music" />
+      </div>
+
+      <button class="button -fill-gradient" type="submit">Submit</button>
+    </form>
+
+    <pre>{{ event }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ],
+      event: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+        pets: 1,
+        extras: {
+          catering: false,
+          music: false
+        }
+      }
+    }
+  }
+}
+</script>
